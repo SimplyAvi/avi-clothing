d0a54bf8652fb59f5c069c0139e094393f2065fa
SimplyAvi <a.totaram@gmail.com>
2020-04-13 17:49:19 -0400

    Fixed label bug on form

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,57 +1,59 @@
-import React from 'react'
+import React from 'react';
 
-import FormInput from '../form-input/form-input.component'
-import './sign-in.styles.scss'
+import FormInput from '../form-input/form-input.component';
+// import CustomButton from '../custom-button/custom-button.component';
 
-class SignIn extends React.Component{
-    constructor(props) {
-        super(props)
+import './sign-in.styles.scss';
 
+class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            email: '',
-            password: ''
-        }
-    }
+    this.state = {
+      email: '',
+      password: ''
+    };
+  }
 
-    handleSubmit = (e) => {
-        e.preventDefault()
-        this.setState({email:'', password:'' })
-    }
-    handleChange = (e) => {
-        const { value, name } = e.target
-        this.setState({[name]: value})
-    }
-    render() {
-        return (
-            <div className='sign-in'>
-                <h2> I already have an account </h2>
-                <span> Sign in with your email and password </span>
+  handleSubmit = event => {
+    event.preventDefault();
 
+    this.setState({ email: '', password: '' });
+  };
 
-                <form onSubmit={this.handleSubmit}>
-                    <FormInput
-                        name='email'
-                        type='email'
-                        handleChange={this.handleChange}
-                        value={this.state.email}
-                        label = 'email'
-                        required
-                    />
-            
-                    <FormInput
-                        name='password'
-                        type='password'
-                        handleChange={this.handleChange}
-                        value={this.state.password}
-                        label = 'password'
-                    />
-                  
-                    <input type='submit' value='Submit Form'/>
-                </form>
-            </div>
-        )
-    }
+  handleChange = event => {
+    const { value, name } = event.target;
+
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <div className='sign-in'>
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            name='email'
+            type='email'
+            handleChange={this.handleChange}
+            value={this.state.email}
+            label='email'
+            required
+          />
+          <FormInput
+            name='password'
+            type='password'
+            value={this.state.password}
+            handleChange={this.handleChange}
+            label='password'
+            required
+          />
+        </form>
+      </div>
+    );
+  }
 }
 
-export default SignIn
+export default SignIn;
--- src/pages/sign-in-sign-up/sign-in-sign-up.component.jsx
+++ src/pages/sign-in-sign-up/sign-in-sign-up.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import SignIn from '../../components/sign-in/sign-in.component'
+
+
 import './sign-in-sign-up.styles.scss'
 
 
