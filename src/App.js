3966bfda7939ca15bf72e621f322744f803dfedc
SimplyAvi <a.totaram@gmail.com>
2020-07-21 05:52:56 -0400

    added checkout page, checkout item component and updated cart to use selectors

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import SignInAndSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up.component'
+import CheckoutPage from './pages/checkout/checkout.component'
+
 import Header from './components/header/header.component'
 import {auth, createUserProfileDocument } from './firebase/firebase.utils'
-import {setCurrentUser} from './redux/user/user.actions'
-
+import { setCurrentUser } from './redux/user/user.actions'
+import  {createStructuredSelector} from 'reselect'
+import { selectCurrentUser } from './redux/user/user.selector'
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
@@ -43,6 +46,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
+          <Route  exact path="/checkout" component={CheckoutPage} />
           <Route
             exact
             path="/signin"
@@ -59,8 +63,8 @@ class App extends React.Component {
     );
   }
 }
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 })
 
 
