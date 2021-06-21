95992ce636e8856267661946cf71030a318f56fc
SimplyAvi <a.totaram@gmail.com>
2021-06-21 15:52:12 -0400

    introduced code splitting for optimal compartment loading using react lazy and suspense

--- client/src/components/custom-button/custom-button.styles.jsx
+++ client/src/components/custom-button/custom-button.styles.jsx
@@ -1,66 +1,45 @@
-import styled, { css } from 'styled-components'
+import styled, { css } from 'styled-components';
 
 const buttonStyles = css`
   background-color: black;
   color: white;
   border: none;
-
-    &:hover {
+  &:hover {
     background-color: white;
     color: black;
     border: 1px solid black;
   }
-`
-
-
-
+`;
 
 const invertedButtonStyles = css`
-{
-     background-color: white;
-     color: black;
-     border: 1px solid black;
-
-     &:hover {
-       background-color: black;
-       color: white;
-       border: none;
-     }
+  background-color: white;
+  color: black;
+  border: 1px solid black;
+  &:hover {
+    background-color: black;
+    color: white;
+    border: none;
   }
-
-`
-
+`;
 
 const googleSignInStyles = css`
-  {
-    background-color: #4285f4;
-    color: white;
-
-    &:hover {
-      background-color: #357ae8;
-      border: none;
-    }
+  background-color: #4285f4;
+  color: white;
+  &:hover {
+    background-color: #357ae8;
+    border: none;
   }
-
-
-`
-
-
+`;
 
 const getButtonStyles = props => {
-    if (props.isGoogleSignIn) {
-        return googleSignInStyles
-    }
+  if (props.isGoogleSignIn) {
+    return googleSignInStyles;
+  }
 
-    return props.inverted? invertedButtonStyles : buttonStyles
-
-}
-
-
-
+  return props.inverted ? invertedButtonStyles : buttonStyles;
+};
 
 export const CustomButtonContainer = styled.button`
-.custom-button {
   min-width: 165px;
   width: auto;
   height: 50px;
@@ -74,6 +53,5 @@ export const CustomButtonContainer = styled.button`
   cursor: pointer;
   display: flex;
   justify-content: center;
-
-    ${getButtonStyles}
-`
+  ${getButtonStyles}
+`;
--- client/src/pages/shop/shop.component.jsx
+++ client/src/pages/shop/shop.component.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { lazy, Suspense} from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
 
-import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
-import CollectionPageContainer from '../collection/collection.container';
+import Spinner from '../../components/spinner/spinner.component'
+const CollectionsOverviewContainer = lazy(()=> import('../../components/collections-overview/collections-overview.containe'))
+const CollectionPageContainer = lazy(()=> import('../collection/collection.container'))
 
 class ShopPage extends React.Component {
   componentDidMount() {
@@ -19,15 +20,17 @@ class ShopPage extends React.Component {
 
     return (
       <div className='shop-page'>
+        <Suspense fallback={<Spinner/>} >
         <Route
-          exact
-          path={`${match.path}`}
-          component={CollectionsOverviewContainer}
+        exact
+        path={`${match.path}`}
+        component={CollectionsOverviewContainer}
         />
         <Route
-          path={`${match.path}/:collectionId`}
-          component={CollectionPageContainer}
+        path={`${match.path}/:collectionId`}
+        component={CollectionPageContainer}
         />
+        </Suspense>
       </div>
     );
   }
